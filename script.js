612e0bc3b4a976fc2a9ce8dd43f63f65bfddf029
Scrooman <kubcwiek@gmail.com>
2025-01-25 13:35:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const wyswietlaczPietra = document.getElementById('wyswietlacz-pietra');
+    const wyswietlaczKierunkuJazdy = document.getElementById('wyswietlacz-kierunku-jazdy');
+    const wyswietlaczPracyDrzwi = document.getElementById('wyswietlacz-pracy-drzwi');
+    const wyswietlaczWartosciPokonanePietra = document.getElementById('wyswietlacz-wartosci-pokonane-pietra');
+
+    // Funkcja do aktualizacji wyświetlaczy
+    function aktualizujWyswietlacze(data) {
+        const lokalizacjaWindy = data.windy_data.lokalizacjaWindy;
+        const kierunekJazdy = data.windy_data.kierunekJazdy;
+
+        // Aktualizacja wyświetlacza kierunku jazdy
+        let kierunekJazdySymbol = '';
+        if (kierunekJazdy === 2) {
+            kierunekJazdySymbol = '↑';
+        } else if (kierunekJazdy === 1) {
+            kierunekJazdySymbol = '↓';
+        } else {
+            kierunekJazdySymbol = ' ';
+        }
+
+        wyswietlaczPietra.textContent = lokalizacjaWindy;
+        wyswietlaczKierunkuJazdy.textContent = kierunekJazdySymbol;
+    }
+
+
+    function aktualizujWyswietlaczeStatystykiWindy(data) {
+        const pokonanePietra = data.pokonane_pietra;
+        wyswietlaczWartosciPokonanePietra.textContent = pokonanePietra;
+    }
+
+    // Funkcja do pobierania danych z serwera
+    function pobierzStatusWindy() {
+        fetch('https://winda.onrender.com/get_winda_status')
+            .then(response => response.json())
+            .then(data => {
+                aktualizujWyswietlacze(data);
+            })
+            .catch(error => console.error('Błąd podczas pobierania danych z serwera:', error));
+    }
+
+    function pobierzStatystykiWindy() {
+        fetch('https://winda.onrender.com/get_statystyki')
+            .then(response => response.json())
+            .then(data => {
+                aktualizujWyswietlaczeStatystykiWindy(data);
+            })
+            .catch(error => console.error('Błąd podczas pobierania danych z serwera:', error));
+    }
+
+
+    setInterval(pobierzStatusWindy, 1000);
+    setInterval(pobierzStatystykiWindy, 1000);
+
+    // Początkowe pobranie danych
+    pobierzStatusWindy();
+    pobierzStatystykiWindy();
+});
