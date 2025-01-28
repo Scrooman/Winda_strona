4ded62a56770bf3fdbf8d16a9c0edb15e38e4457
Scrooman <kubcwiek@gmail.com>
2025-01-28 09:45:08 +0100

    1

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.error('Nie udało się pobrać częstotliwości generowania pasażerów.');
                     return;
                 }
-                suwakCzestotliwosciGenerowaniaWartosc.textContent = suwakCzestotliwosciGenerowania.value;
+                //suwakCzestotliwosciGenerowaniaWartosc.textContent = suwakCzestotliwosciGenerowania.value;
                 
     
                 fetch('https://winda.onrender.com/zmien_czestotliwosc', {
