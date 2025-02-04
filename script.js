58200480ff9eed689be63df8ec920fa51ebf2080
Scrooman <kubcwiek@gmail.com>
2025-02-04 21:05:07 +0100

    1

--- script.js
+++ script.js
@@ -409,8 +409,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     } else if (slownik[idNumber] === 3) {
                         img.src = 'images/panel-pietra-dol.png';
                     } else if (slownik[idNumber] === 1) {
-                        // Jeśli wartość wynosi 1, nie wykonuj żadnej dodatkowej akcji
-                        return
+                        img.src = 'images/panel-pietra.png';
                     }
                 } else {
                     // Wyświetl obrazek panel-pietra.png, jeśli idNumber nie jest w słowniku
